873026ed9d404a73cde8b6e7deceac7a3f0fc431
Shameer-U <shameerushahul@gmail.com>
2018-10-30 13:31:54 +0530

    storing orders

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,10 +4,12 @@ namespace App\Http\Controllers;
 
 use App\Cart;
 use App\Product;
+use App\Order;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use Session;
+use Auth;
 use Stripe\Stripe;
 use Stripe\Charge;
 
@@ -68,18 +70,27 @@ class ProductController extends Controller
 
          Stripe::setApiKey('sk_test_HZrQDqIEHaWTPe9Kl0L7NDX0');
          try{
-            Charge::create([
+            $charge = Charge::create([
                 "amount" => $cart->totalPrice * 100,
                 "currency" => "usd",
                 "source" => "tok_visa",//$request->input('stripeToken'), // obtained with Stripe.js
                 "description" => "Test Charge"
               ]);
+
+              $order = new Order();
+              $order->cart = serialize($cart);
+              $order->address = $request->input('address');
+              $order->name = $request->input('name');
+              $order->payment_id = $charge->id;
+
+              Auth::user()->orders()->save($order);
+
          } catch(\Exception $e){
              return redirect()->route('checkout')->with('error' , $e->getMessage());
 
          }
 
          Session::forget('cart');
-         return redirect()->route('product.index')->with('success', 'Successfully purchased');
+         return redirect()->route('product.index')->with('success', 'Successfully purchased products');
     }
 }
